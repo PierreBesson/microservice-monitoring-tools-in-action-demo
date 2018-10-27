11dec9f6c7698c5be7ec387af76f9d3dd1b95b5d
Pierre Besson <pbesson@ippon.fr>
2018-10-27 17:22:17 +0200

    amend script

--- generate-apps.sh
+++ generate-apps.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # generate jhipster microservices and gateways
-jhipster import-jdl jdl/jhipster-microservice-monitoring-architecture.png
+jhipster import-jdl jdl/microservice-architecture.jdl
 
 # build docker images
 cd store && mvn package -Pprod,zipkin jib:dockerBuild && cd ..
-cd invoice && mvn package -Pprod,zipkin jib:dockerBuild && cd ..
-cd notification && mvn package -Pprod,zipkin jib:dockerBuild && cd ..
+cd crm && mvn package -Pprod,zipkin jib:dockerBuild && cd ..
+cd accountancy && mvn package -Pprod,zipkin jib:dockerBuild && cd ..
